ca10005f5d7b2d10d1b16ee72cccd6f85373d8fa
Evan2698 <zwh2698@163.com>
2026-09-07 18:00:36 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 30
         targetSdk = 33
         versionCode = 1
-        versionName = "1.6"
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
